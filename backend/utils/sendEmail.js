67b83cdcf9c3e0d2a91528dfe2fff4f90db2e1f9
don.technologist <don.technologist@gmail.com>
2024-04-21 15:24:45 +0300

    feat:add send email utility

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import handlebars from "handlebars";
+import path from "path";
+import transporter from "../helpers/emailTransport.js";
+import { systemLogs } from "./logger.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const sendEmail = async (email, subject, payload, template) => {
+  try {
+    const sourceDirectory = fs.readFileSync(
+      path.join(__dirname, template),
+      "utf8"
+    );
+
+    const compiledTemplate = handlebars.compile(sourceDirectory);
+
+    const emailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject,
+      html: compiledTemplate(payload),
+    };
+    await transporter.sendMail(emailOptions);
+  } catch (error) {
+    systemLogs.error(`Error sending email: ${error.message}`);
+  }
+};
+
+export default sendEmail;
