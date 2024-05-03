ab22e0636389192d1f9c0b37291f78997d7e7022
don.technologist <don.technologist@gmail.com>
2024-05-03 10:50:23 +0300

    feat(user): Add getAllUserAccounts feature for admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
+import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
 
+import role from "../middleware/rolesMiddleware.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 
 const router = express.Router();
@@ -10,5 +12,11 @@ const router = express.Router();
 router.get("/profile", checkAuth, getUserProfile);
 router.patch("/profile", checkAuth, updateUserProfile);
 router.delete("/profile", checkAuth, deleteMyAccount);
+router.get(
+  "/all",
+  checkAuth,
+  role.checkRoles(role.ROLES.Admin),
+  getAllUserAccounts
+);
 
 export default router;
